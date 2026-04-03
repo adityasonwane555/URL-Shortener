881526b6ccd30b677f2b07703023098c393cd188
adityasonwane555 <82132527+adityasonwane555@users.noreply.github.com>
2026-04-03 15:15:30 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# URL Shortener
+
+## How does it work
+1. Takes a long URL
+2. Generates a short code
+3. Stores mapping
+4. Retrieves original URL
---
+++ url_shortener.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class URLShortener {
+private:
+    unordered_map<string, string> shortToLong;
+    int counter = 0;
+
+public:
+    string generateShortCode() {
+      counter++;
+      return "url" + to_string(counter);
+    }
+
+    string shortenURL(string longURL) {
+        string shortCode = generateShortCode();
+        shortToLong[shortCode] = longURL;
+        return shortCode;
+    }
+
+    string getOriginalURL(string shortCode) {
+        if (shortToLong.find(shortCode) != shortToLong.end()) {
+            return shortToLong[shortCode];
+        }
+        return "Short code not found.";
+    }
+};
+
+int main() {
+    URLShortener shortener;
+
+    string longURL;
+    cout << "Enter URL: ";
+    cin >> longURL;
+
+    string shortCode = shortener.shortenURL(longURL);
+    cout << "Shortened URL: short.ly/" <<shortCode << endl;
+
+    cout << "Enter short code to retrieve: ";
+    string code;
+    cin >> code;
+
+    cout << "Original URL: " << shortener.getOriginalURL(code) << endl;
+    return 0;
+}
